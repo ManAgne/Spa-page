f1b047b0471c4fd123492e9055f722d57561ad43
managne <agne.manaikaite@gmail.com>
2022-07-18 09:28:11 +0300

    feat: browser router added to app.jsx

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
-import NavBar from '../src/components/navbar'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Navbar from '../src/components/navbar';
+import HomePage from './pages/home-page';
+import TreatmentsPage from './pages/treatments-page';
+import CosmeticsPage from './pages/cosmetics-page';
+import CartPage from './pages/cart-page';
+import ErrorPage from './pages/error-page';
 
-function App() {
+const App = () => {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/treatments-page" element={<TreatmentsPage />} />
+        <Route path="/cosmetics-page" element={<CosmeticsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
