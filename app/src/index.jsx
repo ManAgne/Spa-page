d637d87c00dd7fe5267ecc9eff1bf10a4e056c32
managne <agne.manaikaite@gmail.com>
2022-07-18 15:42:36 +0300

    feat: theme created and primary main color added

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { ThemeProvider } from '@mui/material';
+import theme from './styles/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
