6f118f013cd08df5af53d86f18dda307af706b01
managne <agne.manaikaite@gmail.com>
2022-07-18 13:31:38 +0300

    feat: home-page title added as h1 tag

--- app/src/pages/home-page/index.jsx
+++ app/src/pages/home-page/index.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <Box sx={{ height: '100vh', position: 'relative' }}>
-      <Box component='img' src='bg-image.jpg' sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh', objectFit: 'cover' }} />
+    <Box sx={{ height: '100vh', display: 'flex' }}>
+      <Box component='img' src='bg-image.jpg' sx={{ position: 'absolute', top: '80px', left: 0, width: '100%', height: '100vh', objectFit: 'cover', zIndex: 0}} />
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw', zIndex: 1 }}>
+        <Typography component='h1' variant='h1' sx={{ color: 'white', textAlign: 'center', my: '80px'}}>Cloud 9 Spa</Typography>
+        <Box>afaf</Box>
+      </Box>
     </Box>
   )
 }
