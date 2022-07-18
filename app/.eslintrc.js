c136f93bdfe026a3d540a7011bf481c5b58c78d1
managne <agne.manaikaite@gmail.com>
2022-07-18 20:04:43 +0300

    feat: added 4 new eslint rules

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/prop-types": [0],
+        "eol-last": [1],
+        "no-multiple-empty-lines": [1, { max: 1 }],
+        "react/function-component-definition": [1, {
+            "namedComponents": "arrow-function"
+        }]
     }
 }
