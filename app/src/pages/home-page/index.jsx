e58a8dfa2ac53741bfc6f9aba7b324c5b6612cfb
managne <agne.manaikaite@gmail.com>
2022-07-18 12:58:13 +0300

    feat: background image added to home-page

--- app/src/pages/home-page/index.jsx
+++ app/src/pages/home-page/index.jsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
+import { Box } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box sx={{ height: '100vh', position: 'relative' }}>
+      <Box component='img' src='bg-image.jpg' sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh', objectFit: 'cover' }} />
+    </Box>
   )
 }
 
