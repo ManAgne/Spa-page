ee832fe62ba1cf76e9f91b1d7c8a400630114e87
managne <agne.manaikaite@gmail.com>
2022-08-06 18:37:39 +0300

    refactor: CartProvider moved as separate comp.

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -12,42 +12,25 @@ import CartPage from './pages/cart-page';
 import WishlistPage from './pages/wishlist-page';
 import OnlineReservationPage from './pages/online-reservation-page';
 import ErrorPage from './pages/error-page';
-import CartContext from './contexts/cart-context';
+import { CartProvider } from './contexts/cart-context';
 
-const App = () => {
-  const [cartItems, setCartItems] = React.useState([]);
+const App = () => (
+  <BrowserRouter>
+    <CartProvider>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/treatments" element={<TreatmentsPage />} />
+        <Route path="/cosmetics" element={<CosmeticsPage />} />
+        <Route path="/reservation" element={<OnlineReservationPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/cart" element={<CartPage />} />
 
-  const cartContextValue = React.useMemo(() => ({
-    cartItems,
-    addToCart: (item) => {
-      if (cartItems.find((x) => x.id === item.id)) {
-        setCartItems(cartItems.map((x) => (x.id === item.id
-          ? { ...x, qty: item.qty + 1 }
-          : x)));
-      } else {
-        setCartItems([...cartItems, { item, qty: 1 }]);
-      }
-    },
-  }), [cartItems]);
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
 
-  return (
-    <BrowserRouter>
-      <CartContext.Provider value={cartContextValue}>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/treatments" element={<TreatmentsPage />} />
-          <Route path="/cosmetics" element={<CosmeticsPage />} />
-          <Route path="/reservation" element={<OnlineReservationPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/cart" element={<CartPage />} />
-
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-
-      </CartContext.Provider>
-    </BrowserRouter>
-  );
-};
+    </CartProvider>
+  </BrowserRouter>
+);
 
 export default App;
--- app/src/contexts/cart-context.jsx
+++ app/src/contexts/cart-context.jsx
@@ -2,4 +2,25 @@ import * as React from 'react';
 
 const CartContext = React.createContext();
 
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = React.useState([]);
+
+  const cartContextValue = React.useMemo(() => ({
+    cartItems,
+    addToCart: (item) => {
+      if (cartItems.find((x) => x.id === item.id)) {
+        setCartItems(cartItems.map((x) => (x.id === item.id
+          ? { ...x, qty: item.qty + 1 }
+          : x)));
+      } else {
+        setCartItems([...cartItems, { item, qty: 1 }]);
+      }
+    },
+  }), [cartItems]);
+
+  return (
+    <CartContext.Provider value={cartContextValue}>{children}</CartContext.Provider>
+  );
+};
+
 export default CartContext;
