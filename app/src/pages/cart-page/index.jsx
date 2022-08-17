2e643f0fc9333050e0d761f59001fbd08b7d92b5
managne <agne.manaikaite@gmail.com>
2022-08-17 20:08:14 +0300

    chore: cart page display temporarily moved to separate file and removed from cart-page display

--- app/src/pages/cart-page/index.jsx
+++ app/src/pages/cart-page/index.jsx
@@ -1,34 +1,13 @@
 import * as React from 'react';
-import { Box, Typography } from '@mui/material';
-import { CheckoutSection, ListSection } from './components';
+import { Box } from '@mui/material';
+import CartContext from '../../contexts/cart-context';
 
-const CartPage = () => (
-  <Box sx={{
-    pt: 7, mx: 5, display: 'flex', flexDirection: 'column',
-  }}
-  >
-    <Box
-      component="img"
-      src="bg-image.jpg"
-      sx={{
-        position: 'absolute', top: '80px', left: 0, width: '100%', height: '100vh', objectFit: 'cover', zIndex: 0,
-      }}
-    />
-    <Typography
-      component="h1"
-      variant="h3"
-      align="center"
-      sx={{
-        color: 'white', my: '20px', textShadow: '2px 2px 2px #21313e', zIndex: 1,
-      }}
-    >
-      Your Shopping Bag
-    </Typography>
-    <Box sx={{ display: 'flex', zIndex: 1 }}>
-      <ListSection />
-      <CheckoutSection />
-    </Box>
+const CartPage = () => {
+  const { cartItems } = React.useContext(CartContext);
 
-  </Box>
-);
+  return (
+    <Box component="pre" sx={{ mt: 15 }}>{JSON.stringify(cartItems, null, 4)}</Box>
+  );
+};
+
 export default CartPage;
