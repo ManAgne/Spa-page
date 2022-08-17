9d6cee6a3c35bef6525b7cd3ea9e055aa9c5770e
managne <agne.manaikaite@gmail.com>
2022-08-17 20:09:30 +0300

    refactor: page routes moved to separate file

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,34 +1,16 @@
 import * as React from 'react';
 import {
   BrowserRouter,
-  Routes,
-  Route,
 } from 'react-router-dom';
 import NavBar from './components/navbar';
-import HomePage from './pages/home-page';
-import TreatmentsPage from './pages/treatments-page';
-import CosmeticsPage from './pages/cosmetics-page';
-import CartPage from './pages/cart-page';
-import WishlistPage from './pages/wishlist-page';
-import OnlineReservationPage from './pages/online-reservation-page';
-import ErrorPage from './pages/error-page';
 import { CartProvider } from './contexts/cart-context';
+import PageRoutes from './routes/page-routes';
 
 const App = () => (
   <BrowserRouter>
     <CartProvider>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/treatments" element={<TreatmentsPage />} />
-        <Route path="/cosmetics" element={<CosmeticsPage />} />
-        <Route path="/reservation" element={<OnlineReservationPage />} />
-        <Route path="/wishlist" element={<WishlistPage />} />
-        <Route path="/cart" element={<CartPage />} />
-
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-
+      <PageRoutes />
     </CartProvider>
   </BrowserRouter>
 );
---
+++ app/src/routes/page-routes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import CartPage from '../pages/cart-page';
+import CosmeticsPage from '../pages/cosmetics-page';
+import ErrorPage from '../pages/error-page';
+import HomePage from '../pages/home-page';
+import Login from '../pages/login';
+import Signup from '../pages/signup';
+import TreatmentsPage from '../pages/treatments-page';
+import WishlistPage from '../pages/wishlist-page';
+import OnlineReservationPage from '../pages/online-reservation-page';
+
+const PageRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/treatments" element={<TreatmentsPage />} />
+    <Route path="/cosmetics" element={<CosmeticsPage />} />
+    <Route path="/reservation" element={<OnlineReservationPage />} />
+    <Route path="/wishlist" element={<WishlistPage />} />
+    <Route path="/cart" element={<CartPage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+
+    <Route path="*" element={<ErrorPage />} />
+  </Routes>
+);
+
+export default PageRoutes;
