a657b57f9c4709f5c15f4ad6cbaa11c9f532148a
managne <agne.manaikaite@gmail.com>
2022-08-21 15:29:18 +0300

    feat: login and register form validation completed with formik and yup

--- app/src/pages/login.jsx
+++ app/src/pages/login.jsx
@@ -7,58 +7,106 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import BackgroundImage from '../components/background-image';
 import PageTitle from '../components/page-title';
 
-const Login = () => (
-  <Box sx={{ pt: 7, display: 'flex', flexDirection: 'column' }}>
-    <BackgroundImage />
-    <PageTitle>Existing account</PageTitle>
-    <Paper
-      elevation={3}
-      sx={{
-        mt: 4,
-        p: 3,
-        width: 400,
-        mx: 'auto',
-        bgcolor: 'white',
-        zIndex: 1,
-      }}
-    >
-      <Box
-        component="form"
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const validationSchema = yup.object({
+  email: yup.string()
+    .required('Privaloma')
+    .email('Neteisingas el. pašto formatas'),
+  password: yup.string()
+    .required('Privaloma')
+    .min(8, 'Mažiausiai 8 simboliai')
+    .matches(/[a-z]/, 'Bent viena mažoji raidė')
+    .matches(/[A-Z]/, 'Bent viena didžioji raidė')
+    .matches(/\d/, 'Bent vienas skaičius')
+    .matches(/\W/, 'Bent vienas specialus simbolis'),
+});
+
+const Login = () => {
+  const onSubmit = async (values) => {
+    console.log('Form is confirmed');
+    console.log(values);
+  };
+
+  const {
+    dirty, values, errors, touched, isValid,
+    handleChange, handleBlur, handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Box sx={{ pt: 7, display: 'flex', flexDirection: 'column' }}>
+      <BackgroundImage />
+      <PageTitle>Existing account</PageTitle>
+      <Paper
+        elevation={3}
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: 3,
+          mt: 4,
+          p: 3,
+          width: 400,
+          mx: 'auto',
+          bgcolor: 'white',
+          zIndex: 1,
         }}
       >
-        <TextField
-          name="email"
-          type="email"
-          label="Email"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          name="password"
-          type="password"
-          label="Password"
-          variant="filled"
-          fullWidth
-        />
-        <Button type="submit" variant="contained" size="large">Login</Button>
-        <Typography variant="body2">
-          Do not have an account yet? Register
-          {' '}
-          <RouterLink to="/register">here</RouterLink>
-          .
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 3,
+          }}
+          onSubmit={handleSubmit}
+          disabled={!dirty || !isValid}
+        >
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            variant="filled"
+            fullWidth
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && Boolean(errors.email)}
+            helperText={touched.email && errors.email}
+          />
+          <TextField
+            name="password"
+            label="Password"
+            type="password"
+            variant="filled"
+            fullWidth
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && Boolean(errors.password)}
+            helperText={touched.password && errors.password}
+          />
+          <Button type="submit" variant="contained" size="large">Login</Button>
+          <Typography variant="body2">
+            Do not have an account yet? Register
+            {' '}
+            <RouterLink to="/register">here</RouterLink>
+            .
 
-        </Typography>
-      </Box>
-    </Paper>
-  </Box>
-);
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
 
 export default Login;
--- app/src/pages/register.jsx
+++ app/src/pages/register.jsx
@@ -6,80 +6,156 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Link as RouterLink } from 'react-router-dom';
 import BackgroundImage from '../components/background-image';
 import PageTitle from '../components/page-title';
 
-const Register = () => (
-  <Box sx={{ pt: 7, display: 'flex', flexDirection: 'column' }}>
-    <BackgroundImage />
-    <PageTitle>New account creation</PageTitle>
-    <Paper
-      elevation={3}
-      sx={{
-        mt: 4,
-        p: 3,
-        width: 400,
-        mx: 'auto',
-        bgcolor: 'white',
-        zIndex: 1,
-      }}
-    >
-      <Box
-        component="form"
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+};
+
+const validationSchema = yup.object({
+  firstName: yup.string()
+    .required('Required')
+    .min(3, 'Min 3 letters')
+    .matches(/^[a-ząčęėįšųūž]+$/i, 'Letters only'),
+  lastName: yup.string()
+    .required('Required')
+    .min(3, 'Min 3 letters')
+    .matches(/^[a-ząčęėįšųūž]+$/i, 'Letters only'),
+  email: yup.string()
+    .required('Required')
+    .email('Invalid format'),
+  password: yup.string()
+    .required('Required')
+    .min(8, 'Min 8 characters')
+    .matches(/[a-z]/, 'At least 1 lowercase letter')
+    .matches(/[A-Z]/, 'At least 1 uppercase letter')
+    .matches(/\d/, 'At least 1 number')
+    .matches(/\W/, 'At least 1 special character'),
+  passwordConfirmation: yup.string()
+    .required('Required')
+    .oneOf([yup.ref('password')], 'Passwords do not match'),
+});
+
+const Register = () => {
+  const onSubmit = async (values) => {
+    console.log('Form is confirmed');
+    console.log(values);
+  };
+
+  const {
+    dirty, values, errors, touched, isValid,
+    handleChange, handleBlur, handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Box sx={{ pt: 7, display: 'flex', flexDirection: 'column' }}>
+      <BackgroundImage />
+      <PageTitle>New account creation</PageTitle>
+      <Paper
+        elevation={3}
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: 3,
+          mt: 4,
+          p: 3,
+          width: 400,
+          mx: 'auto',
+          bgcolor: 'white',
+          zIndex: 1,
         }}
       >
-        <TextField
-          name="name"
-          type="text"
-          label="Name"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          name="surname"
-          type="text"
-          label="Surname"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          name="email"
-          type="email"
-          label="Email"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          name="password"
-          type="password"
-          label="Password"
-          variant="filled"
-          fullWidth
-        />
-        <TextField
-          name="passwordConfirmation"
-          type="password"
-          label="Password confirmation"
-          variant="filled"
-          fullWidth
-        />
-        <Button type="submit" variant="contained" size="large">Register</Button>
-        <Typography variant="body2">
-          Already have an account? Login
-          {' '}
-          <RouterLink to="/login">here</RouterLink>
-          .
-
-        </Typography>
-      </Box>
-    </Paper>
-  </Box>
-);
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 3,
+          }}
+          onSubmit={handleSubmit}
+          disabled={!dirty || !isValid}
+        >
+          <TextField
+            name="firstName"
+            label="First name"
+            type="text"
+            variant="filled"
+            fullWidth
+            value={values.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.firstName && Boolean(errors.firstName)}
+            helperText={touched.firstName && errors.firstName}
+          />
+          <TextField
+            name="lastName"
+            label="Last Name"
+            type="text"
+            variant="filled"
+            fullWidth
+            value={values.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.lastname && Boolean(errors.lastName)}
+            helperText={touched.lastName && errors.lastName}
+          />
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            variant="filled"
+            fullWidth
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && Boolean(errors.email)}
+            helperText={touched.email && errors.email}
+          />
+          <TextField
+            name="password"
+            label="Password"
+            type="password"
+            variant="filled"
+            fullWidth
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.password && Boolean(errors.password)}
+            helperText={touched.password && errors.password}
+          />
+          <TextField
+            name="passwordConfirmation"
+            label="Password confirmation"
+            type="password"
+            variant="filled"
+            fullWidth
+            value={values.passwordConfirmation}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.passwordConfirmation && Boolean(errors.passwordConfirmation)}
+            helperText={touched.passwordConfirmation && errors.passwordConfirmation}
+          />
+          <Button type="submit" variant="contained" size="large">Register</Button>
+          <Typography variant="body2">
+            Already have an account? Login
+            {' '}
+            <RouterLink to="/login">here</RouterLink>
+            .
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
 
 export default Register;
