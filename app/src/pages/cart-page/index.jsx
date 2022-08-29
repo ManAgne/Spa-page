2643a8ac7e619f0df9f0947c1c6e3e01d0270741
managne <agne.manaikaite@gmail.com>
2022-08-29 16:36:51 +0300

    refactor: brought back the cart page display view

--- app/src/pages/cart-page/index.jsx
+++ app/src/pages/cart-page/index.jsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
-import CartContext from '../../contexts/cart-context';
+// import CartContext from '../../contexts/cart-context';
+import { ListSection, CheckoutSection } from './components';
+import PageTitle from '../../components/page-title';
 
-const CartPage = () => {
-  const { cartItems } = React.useContext(CartContext);
-
-  return (
-    <Box component="pre" sx={{ mt: 15 }}>{JSON.stringify(cartItems, null, 4)}</Box>
-  );
-};
-
+// const { cartItems } = React.useContext(CartContext);
+const CartPage = () => (
+  <Box sx={{
+    pt: 3, mx: 5, display: 'flex', flexDirection: 'column',
+  }}
+  >
+    <Box
+      component="img"
+      src="bg-image.jpg"
+      sx={{
+        position: 'absolute', top: '80px', left: 0, width: '100%', height: '100vh', objectFit: 'cover', zIndex: 0,
+      }}
+    />
+    <PageTitle>Your Shopping Bag</PageTitle>
+    <Box sx={{ display: 'flex', zIndex: 1 }}>
+      <ListSection />
+      <CheckoutSection />
+    </Box>
+  </Box>
+);
 export default CartPage;
