c4b949e23d26ac296fca4f3f7613939cb363a04a
managne <agne.manaikaite@gmail.com>
2022-10-22 23:57:49 +0300

    feat: created treatments database

--- app/src/pages/treatments-page.jsx
+++ app/src/pages/treatments-page.jsx
@@ -32,8 +32,11 @@ const TreatmentsPage = () => (
                   Treatments
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Est debitis laudantium voluptatibus natus voluptatem.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore qui numquam
+                  quasi eveniet a velit voluptatibus adipisci fugiat omnis, sit, cupiditate,
+                  neque autem soluta ut veritatis. Deserunt expedita dolor, sapiente, dolore
+                  placeat quidem ab quae, sed reprehenderit alias reiciendis cum magni corrupti
+                  praesentium labore aperiam quo eveniet voluptatibus ea. Magni.
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -53,8 +56,11 @@ const TreatmentsPage = () => (
                   Treatments
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Est debitis laudantium voluptatibus natus voluptatem.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aperiam
+                  culpa ab accusamus cumque ducimus tempora quis facilis minus. Commodi quisquam
+                  quibusdam reprehenderit beatae eos eligendi nulla? Eum tempore rem itaque
+                  voluptas ea modi. Alias, voluptate blanditiis ea doloribus corrupti molestiae
+                  ullam obcaecati praesentium totam dolor fugiat quaerat quas sequi!
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -74,8 +80,11 @@ const TreatmentsPage = () => (
                   Treatments
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Est debitis laudantium voluptatibus natus voluptatem.
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem recusandae
+                  officia impedit iure, rem a itaque animi quo laborum, ab veniam eveniet, vero
+                  quaerat delectus deserunt explicabo tenetur excepturi nulla blanditiis. Maiores,
+                  aspernatur! Sapiente, temporibus rerum! Esse aliquam in, rerum nesciunt veniam
+                  quae laborum, sequi perspiciatis accusamus cumque laudantium fuga?
                 </Typography>
               </CardContent>
             </CardActionArea>
