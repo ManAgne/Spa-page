79432ebf6ba92e608098e5988e233a4220627de6
managne <agne.manaikaite@gmail.com>
2022-10-23 18:02:23 +0300

    feat: services data fetch created and implemented into accordion under separate treatment pages

--- app/src/pages/rituals.jsx
+++ app/src/pages/rituals.jsx
@@ -9,47 +9,75 @@ import {
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { PageTitle } from '../components';
-// const [treatments, setTreatments] = React.useState([]);
 
-// React.useEffect(() => {
-//   fetch('http://localhost:8000/treatments')
-//     .then((res) => res.json())
-//     .then((fetchedTreatments) => setTreatments(fetchedTreatments));
-// }, []);
+const RitualsPage = () => {
+  const [treatments, setTreatments] = React.useState([]);
+  const [expanded, setExpanded] = React.useState(false);
 
-/* {treatments.filter(treatment => treatment.title === 'rituals') => ( */
+  React.useEffect(() => {
+    fetch('http://localhost:8000/services')
+      .then((res) => res.json())
+      .then((fetchedTreatments) => setTreatments(fetchedTreatments));
+  }, []);
 
-const RitualsPage = () => (
-  <Box sx={{
-    display: 'flex', flexDirection: 'column', alignContent: 'center', pt: 3,
-  }}
-  >
-    <Box
-      component="img"
-      src="https://images.unsplash.com/photo-1528715471579-d1bcf0ba5e83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1793&q=80.jpg"
-      sx={{
-        position: 'absolute', top: '80px', left: 0, width: '100%', height: '100vh', objectFit: 'cover', zIndex: 0,
-      }}
-    />
-    <PageTitle>Spa Rituals</PageTitle>
-    <Card sx={{
-      display: 'flex', justifyContent: 'center', alignSelf: 'center', maxWidth: '1500px', p: 5, zIndex: 1,
+  const rituals = treatments.filter((treatment) => treatment.category === 'ritual');
+  const handleChange = (panel) => (isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <Box sx={{
+      display: 'flex', flexDirection: 'column', alignContent: 'center', pt: 3,
     }}
     >
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>title</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>description</Typography>
-        </AccordionDetails>
-      </Accordion>
-    </Card>
-  </Box>
-);
+      <Box
+        component="img"
+        src="https://images.unsplash.com/photo-1528715471579-d1bcf0ba5e83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1793&q=80.jpg"
+        sx={{
+          position: 'absolute', top: '80px', left: 0, width: '100%', height: '100vh', objectFit: 'cover', zIndex: 0,
+        }}
+      />
+      <PageTitle>Spa Rituals</PageTitle>
+      <Card sx={{
+        display: 'flex', flexDirection: 'column', alignContent: 'center', alignSelf: 'center', maxWidth: '1000px', p: 5, zIndex: 1,
+      }}
+      >
+        {rituals.map(({
+          id,
+          title,
+          description,
+          duration,
+          price,
+        }) => (
+
+          <Accordion
+            key={id}
+            expanded={expanded === id}
+            onChange={handleChange(id)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+              sx={{ display: 'flex ', justifyContent: 'space-between' }}
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>{title}</Typography>
+              <Typography>
+                {duration}
+                {' '}
+                /
+                {' '}
+                {price}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{description}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Card>
+    </Box>
+  );
+};
 
 export default RitualsPage;
