61666be1999c42edd0071728c1285eaa3f8e389a
managne <agne.manaikaite@gmail.com>
2022-10-23 18:35:17 +0300

    feat: theme breakpoints and mixin themes added

--- app/src/styles/theme.js
+++ app/src/styles/theme.js
@@ -1,11 +1,46 @@
 import { createTheme } from '@mui/material';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#21313e',
+const baseTheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 780,
+      lg: 992,
+      xl: 1200,
+      xxl: 1536,
+    },
+  },
+
+  shape: {
+    borderRadius: 4,
+  },
+
+  zIndex: {
+    appBar: 1300,
+  },
+});
+
+const mixinTheme = createTheme(baseTheme, {
+  mixins: {
+    navbar: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      height: '64px',
+      padding: baseTheme.spacing(0, 2),
     },
   },
 });
 
-export default theme;
+const createColor = (color) => mixinTheme.palette.augmentColor({ color: { main: color } });
+
+const lightTheme = createTheme(mixinTheme, {
+  palette: {
+    primary: {
+      ...createColor('#21313e'),
+      contrastText: baseTheme.palette.common.white,
+    },
+  },
+});
+
+export default lightTheme;
