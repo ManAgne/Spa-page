d00d59bc2a2c5b083f7ae3bb1fe07400a3ff5043
managne <agne.manaikaite@gmail.com>
2022-10-24 12:34:06 +0300

    feat: cart item count calculation added to cart context

--- app/src/contexts/cart-context.jsx
+++ app/src/contexts/cart-context.jsx
@@ -17,7 +17,9 @@ export const CartProvider = ({ children }) => {
       }
     },
 
-    getSubtotal: () => cartItems.reduce((prevSum, { count, price }) => prevSum + count * price, 0),
+    cartItemsCount: cartItems.reduce((sum, { count }) => sum + count, 0),
+
+    getSubtotal: cartItems.reduce((prevSum, { count, price }) => prevSum + count * price, 0),
 
     deleteItem: (id) => setCartItems(cartItems.filter((x) => x.id !== id)),
   }), [cartItems]);
